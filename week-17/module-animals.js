ebb95955bf15a3cc5cd6e006ea478ca18d2ed6cf
Roger Nguyen <quanghuy1811992@gmail.com>
2024-07-18 19:32:46 +1000

    commit all practice

---
+++ week-17/module-animals.js
@@ -0,0 +1,8 @@
+import Dog from './module-dog.js';
+
+let animal1 = new Dog('dog-1');
+let animal2 = new Dog('dog-2');
+
+let animalCollection = [animal1, animal2];
+
+export default animalCollection;
---
+++ week-17/module-main.js
@@ -0,0 +1,6 @@
+import Dog from './module-dog.js';
+import animalCollection from './module-animals.js';
+
+window.puppy = new Dog('puppy');
+
+console.log(animalCollection);

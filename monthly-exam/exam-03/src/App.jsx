79187b3506ce669bf605d0d74fab9c287ad42508
Roger Nguyen <quanghuy1811992@gmail.com>
2024-10-24 20:39:04 +1100

    exam

--- monthly-exam/exam-03/src/App.jsx
+++ monthly-exam/exam-03/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
-
+import '@fontsource-variable/suse';
 function App() {
   const [count, setCount] = useState(0);
 
@@ -17,19 +17,28 @@ function App() {
         <div className="text">
           <div>
             <span className="big-m"></span>
-            ax had always dreamed of the perfect Sunday: cozy bed, snacks within
-            reach, and every streaming service at his fingertips. Today, that
-            dream was finally coming true. He had stocked up on popcorn, candy,
-            and enough soda to float a small boat. Wrapped in his softest
-            blanket, he flicked on the TV and sank into the cushions. "This," he
-            thought, "is heaven."
+            <p>
+              ax had always dreamed of the perfect Sunday: cozy bed, snacks
+              within reach, and every streaming service at his fingertips.
+              Today, that dream was finally coming true. He had stocked up on
+              popcorn, candy, and enough soda to float a small boat.
+            </p>
+            <p>
+              Wrapped in his softest blanket, he flicked on the TV and sank into
+              the cushions. "This," he thought, "is heaven."
+            </p>
           </div>
           <div>
-            Just as Max found the perfect movie, his phone buzzed. It was a
-            message from his friend Lisa: "Ready for our 5K run?" Max's eyes
-            widened in horror. He had completely forgotten! With a groan, he
-            looked from the TV to his running shoes by the door. "Well," he
-            sighed, "it seemed like heaven... while it lasted!"
+            <p>
+              Just as Max found the perfect movie, his phone buzzed. It was a
+              message from his friend Lisa: "Ready for our 5K run?" Max's eyes
+              widened in horror.
+            </p>
+            <p>
+              He had completely forgotten! With a groan, he looked from the TV
+              to his running shoes by the door. "Well," he sighed, "it seemed
+              like heaven... while it lasted!"
+            </p>
           </div>
         </div>
       </div>
